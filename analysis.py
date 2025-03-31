ac0fa9e932baf11de40b0c14eaa2d6828e065624
bg20061220 <1goelbha@hdsb.ca>
2025-03-30 21:21:34 -0400

    Started Data Kind
    Downloaded and Completed Setup, need to proceed to analysis next time.

---
+++ analysis.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv("cars24data.csv") 
+# print(df.info())
+print(df.isnull().sum())
+# Remove duplicate rows
+
