c496fdf2f4341bf48d27ef88a849a2fc1022bc4a
bg20061220 <1goelbha@hdsb.ca>
2025-07-09 17:05:46 -0400

    fixed all errors

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from train_model import ImperfectionBinner
 # Load the model
 model = joblib.load("model.pkl")
 
-st.title("🚗 Second-hand Car Price Predictor")
+st.title("🚗 Second-hand Car Price Predictor (Only For Maruti Cars)")
 
 # User input
 year = st.number_input("Manufacturing Year", min_value=1990, max_value=2025)
@@ -18,6 +18,8 @@ owners = st.selectbox("Number of Owners", [1, 2, 3])
 fuel = st.selectbox("Fuel Type", ["Petrol", "Diesel", "CNG"])
 imperfections = st.slider("Imperfections (0 = None, 10 = Worst)", 0, 10)
 repainted = st.slider("Repainted Parts (0 = None, 10 = All)", 0, 10)
+Car_Age = 2025 - year  # Calculate Car Age
+KM_per_Year = km / Car_Age # Calculate KM per Year
 
 # Predict
 if st.button("Predict Price"):
@@ -30,7 +32,9 @@ if st.button("Predict Price"):
         "Ownership": owners,
         "Fuel type": fuel,
         "Imperfections": imperfections,
-        "Repainted Parts": repainted
+        "Repainted Parts": repainted,
+        "KM_per_Year" : KM_per_Year,
+        "Car_Age": Car_Age
     }])
     
     prediction = model.predict(input_df)[0]
